6dda691dd585ec73a4b0bdbd765a63d9c04d83ef
Edsel <pretxel@hotmail.com>
2023-07-06 21:34:02 +0100

    add analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,12 @@
 import { RecoilRoot } from 'recoil'
+import { Analytics } from '@vercel/analytics/react';
+
 
 export default function MyApp({ Component, pageProps }) {
     return (
         <RecoilRoot>
             <Component {...pageProps} />
         </RecoilRoot>
+        <Analytics />
     )
 }
